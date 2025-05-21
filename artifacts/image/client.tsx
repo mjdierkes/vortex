27f17895b0a36d5404548956447635c6297dd3d8
mjdierkes <mjdierkes@msn.com>
2025-05-20 19:36:38 -0700

    dont auto open

--- artifacts/image/client.tsx
+++ artifacts/image/client.tsx
@@ -11,7 +11,7 @@ export const imageArtifact = new Artifact({
       setArtifact((draftArtifact) => ({
         ...draftArtifact,
         content: streamPart.content as string,
-        isVisible: true,
+        // isVisible: true,
         status: 'streaming',
       }));
     }
